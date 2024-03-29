9e586b41f0dcb79a9d9240ceb757bff02e7a65b3
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-03-30 03:15:37 +0530

    Week-10

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from Student import Student
+from Employee import Employee
+from Person import Person
+
+P = Person('Sunil Dutt',55)
+P.display()
+
+S = Student('Rajesh',29,87)
+S.display()
+
+E = Employee('Sujit Malhotra',55,300000)
+E.display()
