0f3a88bf5c2a5d70d4535312d60832bd092c1afe
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-03-30 06:36:05 +0530

    Week-10

--- Person.py
+++ Person.py
@@ -1,8 +1,9 @@
-
+# If you remove the commented code in fifth line the code will not work
 class Person():
     def __init__(self, name, age):
         self.name = name
-        self.age = age
-    
+        #self.__age = age        # Will not work as we are declearing age as a private variable by using double underscore '__' 
+        self.age = age                        # before the variable so the child class won't be able to access the variable
+                                # Will produce AttributeError: 'Person' object has no attribute 'age'
     def display(self):
         print(self.name,self.age)   
