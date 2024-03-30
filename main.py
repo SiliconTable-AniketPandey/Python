ec2c2d39cb86e43898ae06098955d321dfdfeaf6
TheFatCow <50284597+TheFatCow@users.noreply.github.com>
2024-03-30 05:38:30 +0530

    week-10

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from Person import Person
 P = Person('Sunil Dutt',55)
 P.display()
 
-S = Student('Rajesh',29,87)
+S = Student('Aniket',18,200)
 S.display()
 
-E = Employee('Sujit Malhotra',55,300000)
+E = Employee('Rayan',55,100000.55)
 E.display()
